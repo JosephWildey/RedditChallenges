7954025408c2e09b074f57cd5079dcbc5e4a3771
Joseph Wildey <joewildey@outlook.com>
2019-03-03 14:42:56 -0500

    Update AdditivePersistence.java

--- AdditivePersistence.java
+++ AdditivePersistence.java
@@ -18,25 +18,37 @@ public class AdditivePersistence {
 
         /*Gotta init our array. */
         char[] arr = new char[len];
-
+        
+        /*Gotta flesh out the array with values. */
         for(int i = 0; i < len; ++i)
             arr[i] = number.charAt(i);
 
+        /*Might need more than one iteration. */
         while(len > 1) {
             int num = 0;
             char num2;
 
+            /*Loop  through the array we've created and add values
+            as we go through it.*/
             for(int i = 0; i < len; ++i) {
                 num2 = arr[i];
                 num += Character.getNumericValue(num2);
             }
 
+            /*This makes it easier to determine the length of the value.*/
             String numStr = Integer.toString(num);
 
+            /*Output the value.*/
             System.out.println(numStr);
 
+            /*Just in case the number is greater than one digit in length.
+            May as well convert it to a num now.*/
             num = Integer.parseInt(numStr);
 
+            /*If we can we'll break it now. Otherwise, we'll repopulate the array
+            with the old values. To avoid going beyond the scope of the string
+            we'll set the length to it's new value. Anything beyond this point
+            will be ignored later on too.*/
             if(numStr.length() == 1) {
                 break;
             } else {
