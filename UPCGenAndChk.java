1669ffb66f6a078321dfb4b2192c42671c3a99d9
Joseph Wildey <joewildey@outlook.com>
2019-03-09 10:21:17 -0500

    Update UPCGenAndChk.java
    Added a function to return a boolean value to evaluate the UPC and possibly stop it from being output if it is invalid. Now all that is really left is to allow the user to input their own 12 digit UPC and check its validity.

--- UPCGenAndChk.java
+++ UPCGenAndChk.java
@@ -44,10 +44,51 @@ public class UPCGenAndChk {
         else
             arr[11] = M;
 
-        /*Display the UPC.*/
-        System.out.print("The UPC is: ");
-        for(int i = 0; i < 12; ++i) {
-            System.out.print(arr[i]);
+        /* If something messes up it won't show a possibly incorrect
+        UPC. Rather, it'll just tell the user. */
+        if(checkUPC(arr) == true) {
+            System.out.print("The UPC is: ");
+            for(int i = 0; i < 12; ++i) {
+                System.out.print(arr[i]);
+            }
+        } else {
+            System.out.print("Sorry, an invalid was generated!");
+        }
+    }
+
+    /* Basically the same thing as generating a UPC. Except we are not
+    altering the array. Just computing and checking the digit bit with
+    what we would expect. */
+    public static boolean checkUPC(int[] arr) {
+        int num = 0, evenSum = 0, oddSum = 0, M = 0;
+        for(int i = 0; i < 11; ++i) {
+            if((i+1) % 2 == 0)
+                evenSum += arr[i];
+            else if((i+1) % 2 == 1)
+                oddSum += arr[i];
+        }
+
+        /*Once the loop is complete we multiply the sum of odd indexes by 3.*/
+        oddSum *= 3;
+
+        /*Then we add the sum of even indexes.*/
+        oddSum += evenSum;
+
+        /*We need M to equal modulo 10 of the last operation.*/
+        M = oddSum % 10;
+
+        /*The value of the check digit bit depends on the value of M.
+        If M is zero, then that digit is 0. If M is not 0, it is 10 - M. */
+        M = 10 - M;
+
+        /* Since the function is not a 'void' a return value is expected.
+        The function is just checking the validity of the UPC so boolean
+        makes sense as a return function, and if the UPC is valid it'll
+        return true. */
+        if(arr[11] == M) {
+            return true;
+        } else {
+            return false;
         }
     }
 }
