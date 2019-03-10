ab9a41460ca47d8a7ab810e61172a547a127555a
Joseph Wildey <joewildey@outlook.com>
2019-03-10 10:59:21 -0400

    Update AdditivePersistence.java
    Efficiency through recursion. :)

--- AdditivePersistence.java
+++ AdditivePersistence.java
@@ -1,12 +1,9 @@
-/*I cheated and used strings. Just a head's up.*/
-
 import java.util.Scanner;
 
 public class AdditivePersistence {
 
     public static void main(String[] args) {
-        /*Gotta set ourselves up for success. The counter must be initialized up here
-        otherwise each iteration it'll revert back to its initial value due to scope.*/
+        /*Gotta set ourselves up for success.*/
         String number;
         int len = 0, counter = 1;
         Scanner strInput = new Scanner(System.in);
@@ -21,47 +18,37 @@ public class AdditivePersistence {
 
         /*Gotta init our array. */
         char[] arr = new char[len];
-        
+
         /*Gotta flesh out the array with values. */
         for(int i = 0; i < len; ++i)
             arr[i] = number.charAt(i);
 
-        /*Might need more than one iteration. */
-        while(len > 1) {
-            int num = 0;
-            char num2;
+        System.out.println(addDigits(arr));
+    }
 
-            /*Loop  through the array we've created and add values
-            as we go through it.*/
-            for(int i = 0; i < len; ++i) {
+    public static int addDigits(char[] arr) {
+        int num = 0, counter = 0, len = arr.length;
+        char num2;
+        for(int i = 0; i < len; ++i) {
                 num2 = arr[i];
                 num += Character.getNumericValue(num2);
             }
-
-            /*This makes it easier to determine the length of the value.*/
-            String numStr = Integer.toString(num);
-
-            /*Output the value.*/
-            System.out.println(numStr);
-
-
-            /*If we can we'll break it now. Otherwise, we'll repopulate the array
-            with the old values. To avoid going beyond the scope of the string
-            we'll set the length to it's new value. Anything beyond this point
-            will be ignored later on too. We'll also convert the string object
-            to an int here, but only if necessary. We'll also keep track of the 
-            additive persistence value here.*/
-            if(numStr.length() == 1) {
-                System.out.println("The additive persistence is: " + counter);
-                break;
-            } else {
-                num = Integer.parseInt(numStr);
-                len = numStr.length();
-                for(int i = 0; i < len; ++i) {
-                    arr[i] = numStr.charAt(i);
-                }
-                ++counter;
-            }
+            ++counter;
+            System.out.println(num);
+        if(len > 1) {
+            addDigits(arr, counter);
+            ++counter;
         }
+        return counter;
+    }
+
+    public static void addDigits(char[] arr, int counter) {
+        int num = 0, len = arr.length;
+        char num2;
+        for(int i = 0; i < len; ++i) {
+                num2 = arr[i];
+                num += Character.getNumericValue(num2);
+            }
+        ++counter;
     }
 }
