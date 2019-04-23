b4b6f7af163a8363ef5c271e75c8265375257978
Joseph Wildey <joewildey@outlook.com>
2019-04-23 13:09:56 -0400

    Update NumProcessor.java

--- NumProcessor.java
+++ NumProcessor.java
@@ -12,11 +12,19 @@ class NumProcessor {
         System.out.print("Please input a number: ");
         processedNum = usrNum.nextInt();
 
+        /*Using mod we get the number in reverse order, but
+        we're able to do this on a per-digit basis. Then we just
+        add one to each digit when inputting it into the stack.
+        We need to use a stack to retrieve the numbers in reverse order
+        due to using mod. */
         while(processedNum > 0) {
             nums.push(processedNum % 10 + 1);
             processedNum /= 10;
         }
 
+        /* Items popped from the top of stack are removed from the
+        stack. Storing them as an int allows us to recreate the number
+        that was initially input with 1s added to each digit. */
         while(!nums.empty()) {
             outputNum = nums.pop();
             System.out.print(outputNum);
