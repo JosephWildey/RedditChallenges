75ed2ab5baafdf94e0bf01e27822ea3a2dd9a7c4
Joseph Wildey <joewildey@outlook.com>
2019-06-11 07:58:54 -0400

    Update UPCGenAndChk.java
    Cleaner and more coherent code.

--- UPCGenAndChk.java
+++ UPCGenAndChk.java
@@ -3,143 +3,126 @@ import java.util.Scanner;
 
 public class UPCGenAndChk {
 
-    /*Technically you want as little code as possible here for
-    best performance.*/
+    /** Variable to store the user's choice when prompted. */
+    private static int choice = 0;
+
+    /** Long array to store all the values of the generated or provided 12-integer UPC. */
+    final static long[] UPC_ARRAY = new long[12];
+
+    /** Long to retrieve the UPC when provided by the user, because it's 12 digits large. */
+    private static long userUPC = 0;
+
     public static void main(String[] args) {
-        menu();
+        Menu();
     }
 
-    public static void GenUPC() {
-        int num = 0, evenSum = 0, oddSum = 0, M = 0;
-        Random rand = new Random(System.currentTimeMillis());
+    /**
+     * Prompts the user to make a choice.
+     * @param - none
+     * @return - none
+    */
+    public static void Menu() {
+        Scanner userInput = new Scanner(System.in);
 
-        int[] arr = new int[12];
+        System.out.print("Enter 1 to generate a UPC or 2 to check a UPC: ");
+        choice = userInput.nextInt();
 
-        /* Fill the integer array with 11 random digits between 0 and 9. */
-        for(int i = 0; i < 11; ++i) {
-            num = rand.nextInt(10);
-            arr[i] = num;
-        }
-
-        for(int i = 0; i < 11; ++i) {
-            if((i+1) % 2 == 0)
-                evenSum += arr[i];
-            else if((i+1) % 2 == 1)
-                oddSum += arr[i];
-        }
-
-        /*Once the loop is complete we multiply the sum of odd indexes by 3.*/
-        oddSum *= 3;
-
-        /*Then we add the sum of even indexes.*/
-        oddSum += evenSum;
-
-        /*We need M to equal modulo 10 of the last operation.*/
-        M = oddSum % 10;
-
-        /*The value of the check digit bit depends on the value of M.
-        If M is zero, then that digit is 0. If M is not 0, it is 10 - M. */
-        if(M != 0)
-            arr[11] = 10 - M;
-        else
-            arr[11] = M;
-
-        /* If something messes up it won't show a possibly incorrect
-        UPC. */
-        if(checkUPC(arr) == true) {
-            System.out.print("The UPC is: ");
-            for(int i = 0; i < 12; ++i) {
-                System.out.print(arr[i]);
-            }
-        } else {
-            //Recursion in the case of failure. User won't know if the program ever failed.
-            GenUPC();
-        }
-    }
-
-    /* Basically the same thing as generating a UPC. Except we are not
-    altering the array. Just computing and checking the digit bit with
-    what we would expect. */
-    public static boolean checkUPC(int[] arr) {
-        int num = 0, evenSum = 0, oddSum = 0, M = 0;
-        for(int i = 0; i < 11; ++i) {
-            if((i+1) % 2 == 0)
-                evenSum += arr[i];
-            else if((i+1) % 2 == 1)
-                oddSum += arr[i];
-        }
-
-        /*Once the loop is complete we multiply the sum of odd indexes by 3.*/
-        oddSum *= 3;
-
-        /*Then we add the sum of even indexes.*/
-        oddSum += evenSum;
-
-        /*We need M to equal modulo 10 of the last operation.*/
-        M = oddSum % 10;
-
-        /*The value of the check digit bit depends on the value of M.
-        If M is zero, then that digit is 0. If M is not 0, it is 10 - M. */
-        M = 10 - M;
-
-        /* Since the function is not a 'void' a return value is expected.
-        The function is just checking the validity of the UPC so boolean
-        makes sense as a return function, and if the UPC is valid it'll
-        return true. */
-        if(arr[11] == M) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    /* Generates output based on user input while minimizing code present
-    in the main function. Making the program prettier.*/
-    public static void menu() {
-        /* Setting ourselves up for success */
-        Scanner usrInput = new Scanner(System.in);
-        Scanner UPCInput = new Scanner(System.in);
-        /* The use of a string prohibits inputs like "Pizza" without throwing an exception
-        that I cannot seem to catch with a try-catch statement. */
-        String usrChoice = "0";
-        int[] arr = new int[12];
-        String usrUPC;
-
-        /* Get the user's choice */
-        System.out.print("Enter 1 to generate a UPC or 2 to check your 12-digit UPC: ");
-        usrChoice = usrInput.nextLine();
-
-        /* Exact equivalence works here. */
-        switch(usrChoice) {
+        switch(choice) {
         case 1:
-            GenUPC();
+            GenerateUPC();
+            DisplayUPCArray();
             break;
         case 2:
-            System.out.print("Please input your UPC: ");
-            usrUPC = UPCInput.nextLine();
-            ChkUsrUPC(usrUPC);
+            System.out.print("Please enter your UPC: ");
+            userUPC = userInput.nextLong();
+            CheckUPC(userUPC);
             break;
         default:
-            System.out.println("Sorry, please try again.");
-            menu();
-            break;
+            System.out.print("Sorry, please try again.");
+            Menu();
         }
     }
 
-    /* This will check a UPC that a user inputs. Basically just asks for the UPC,
-    stores it as a string, then converts it to an array of ints. Super ugly tho. */
-    public static void ChkUsrUPC(String UPC) {
-        int[] upcNums = new int[12];
 
-        for(int i = 0; i < 12; ++i) {
-            //Ugly beast.
-            upcNums[i] = Character.getNumericValue(UPC.charAt(i));
+    /** This method generates a UPC, including the final digit based on previous digits.
+     * @param - none
+     * @return - true
+    */
+    public static void GenerateUPC() {
+        long evenSum = 0, oddSum = 0, checkBit = 0;
+        int num = 0;
+        Random randGen = new Random(System.currentTimeMillis());
+
+        for(int i = 0; i < 11; ++i) {
+            num = randGen.nextInt(10);
+            if (i % 2 == 0) {
+                UPC_ARRAY[i] = num;
+                evenSum += num;
+            } else {
+                UPC_ARRAY[i] = num;
+                oddSum += num;
+            }
         }
 
-        if(checkUPC(upcNums) == true) {
-            System.out.print("The UPC is valid.");
-        } else {
-            System.out.print("Invalid UPC.");
+        checkBit = (oddSum * 3 + evenSum) % 10;
+
+        if(checkBit != 0)
+            checkBit = 10 - checkBit;
+
+        UPC_ARRAY[11] = checkBit;
+    }
+
+    /**
+     * This will check the UPC provided by the user.
+     * @param - UPC, UPC to be broken up to be stored into the array and checked for validity.
+     * @return - false
+     *
+    */
+    public static void CheckUPC(long UPC) {
+        long evenSum = 0, oddSum = 0, checkBit = 0;
+        for(int i = 11; i >= 0; --i) {
+            if((UPC % 10) % 2 == 0) {
+                UPC_ARRAY[i] = UPC % 10;
+                UPC = UPC / 10;
+            } else {
+                UPC_ARRAY[i] = UPC % 10;
+                UPC = UPC / 10;
+            }
         }
+
+        for(int i = 0; i < 11; ++i) {
+            if(i % 2 == 0)
+                evenSum += UPC_ARRAY[i];
+            else
+                oddSum += UPC_ARRAY[i];
+        }
+
+            checkBit = (oddSum * 3 + evenSum) % 10;
+
+            if(checkBit != 0)
+                checkBit = 10 - checkBit;
+
+            if(checkBit == UPC_ARRAY[11])
+                System.out.print("The UPC is valid.");
+            else
+                System.out.print("The UPC is invalid.");
+    }
+
+    /** Gets the user UPC.
+     * @param - none
+     * @return - userUPC, the UPC that was fed into the array.
+    */
+    public static long getUPC() {
+            return userUPC;
+    }
+
+    /**
+    * Returns the UPC Array when a UPC is generated instead of supplied.
+    * @param - none
+    * @return - none
+    */
+    public static void DisplayUPCArray() {
+        for(int i = 0; i <= 11; ++i)
+            System.out.print(UPC_ARRAY[i]);
     }
 }
