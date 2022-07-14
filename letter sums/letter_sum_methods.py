0248b9aecb6330d6a30f5f92e692ad09a48db4f5
Joseph Wildey <joewildey@outlook.com>
2022-07-14 09:45:50 -0400

    Update letter_sum_methods.py
    Removed empty lists and applied a divide and conquer approach to each solution.

--- letter sums/letter_sum_methods.py
+++ letter sums/letter_sum_methods.py
@@ -2,15 +2,11 @@ from __future__ import annotations
 from statistics import mode
 
 def compute_score(word: str) -> str:
-
-    word_score = 0
-
     word_score = sum([ord(letter.lower()) - 96 for letter in word])
 
     return word_score
 
 def find_highest_score(words: list) -> list:
-
     midpoint = len(words) // 2
     
     left_side = [compute_score(word) for word in words[:midpoint]]
@@ -23,16 +19,19 @@ def find_highest_score(words: list) -> list:
          return max(right_side)
 
 def find_odd_sums(words: list) -> list:
+    midpoint = len(words) // 2
 
-    odd_score_words = []
-    
-    odd_score_words = [word for word in words if compute_score(word) % 2 == 1]
-    
-    return len(odd_score_words)
+    left_side = words[:midpoint]
+
+    right_side = words[midpoint:]
+
+    odd_word_scores = [word for word in left_side if compute_score(word) % 2 == 1]
+
+    odd_word_scores = [word for word in right_side if compute_score(word) % 2 == 1]
+
+    return len(odd_word_scores)
 
 def find_words_mode(words: list) -> list:
-    word_scores = []
-    
     midpoint = len(words) // 2
     
     left_side = words[:midpoint]
