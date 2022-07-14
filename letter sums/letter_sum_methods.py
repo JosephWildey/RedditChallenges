c5bdd1d248d9c79c98170397ab5e15acbe9b35dc
Joseph Wildey <joewildey@outlook.com>
2022-07-14 08:26:18 -0400

    Update letter_sum_methods.py
    Removed "letter_count" method because compute_score is much more concise and does the exact same thing.

--- letter sums/letter_sum_methods.py
+++ letter sums/letter_sum_methods.py
@@ -31,27 +31,6 @@ def find_odd_sums(word_list):
 
     print("Words with odd scores: " + str(len(odd_counts)))
 
-# Simple method that provides a value between 1 and 26 for each letter in the alphabet and sums the value.
-def letter_Count(word):
-
-# intiate the sum variable
-
-    letterSum = 0
-
-# Loop through the provided word in the argument, if a non-alpha character is caught raise an exception.
-# Otherwise return the sum of the letters.
-    for letter in word:
-
-        # simple check to make sure the ascii value of each character is within the range of lowercase letters
-        if ord(letter.lower()) < 97 or ord(letter.lower()) > 122:
-            raise Exception("Invalid character detected.")
-        # if every character is a letter add up the values to be returned to the user
-        else:
-            letterSum += ord(letter.lower()) - 96
-
-# output sum of letter values if an exception has not been raised
-    print(word + " has a score of: " + str(letterSum))
-
 def find_words_mode(word_list):
 
     # This will be used for storing a word's score later on
