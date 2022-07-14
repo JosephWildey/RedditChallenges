a54d4245ef7c08f446e12fe5c92e10a67ff4cd7c
Joseph Wildey <joewildey@outlook.com>
2022-07-14 09:14:49 -0400

    Update main.py
    Fixed one of the import statements

--- letter sums/main.py
+++ letter sums/main.py
@@ -1,4 +1,4 @@
-from letter_sum_methods import letter_Count
+from letter_sum_methods import compute_score
 from letter_sum_methods import find_highest_score
 from letter_sum_methods import find_odd_sums
 from letter_sum_methods import find_words_mode
@@ -16,8 +16,3 @@ if __name__ == '__main__':
     find_highest_score(file_words)
 
     find_odd_sums(file_words)
-
-# grab user input
-    user_word = input("Enter a word: ")
-# process user input
-    letter_Count(user_word)
