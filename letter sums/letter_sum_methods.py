411c9fbc1628f4879c6483261fb00c264b62ddd2
Joseph Wildey <joewildey@outlook.com>
2022-07-14 08:42:48 -0400

    Update letter_sum_methods.py
    I changed the "find_highest_score" method to use a list comprehension and built-in methods.

--- letter sums/letter_sum_methods.py
+++ letter sums/letter_sum_methods.py
@@ -2,18 +2,11 @@ from collections import defaultdict
 
 def find_highest_score(word_list):
 
-    letterSum = 0
-
-    for word in word_list:
-        letterSum = compute_score(word)
-
-        if letterSum > bestScore:
-            bestScore = letterSum
-            bestWord = word
-
-        letterSum = 0
-
-    print(bestWord + " " + str(bestScore))
+    word_scores = []
+    
+    word_scores = [compute_score(word) for word in words]
+    
+    return max(word_scores)
 
 def find_odd_sums(word_list):
 
